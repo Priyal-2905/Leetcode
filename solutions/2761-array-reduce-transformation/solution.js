242596a6faadd091476af227be57b612fdd2dcff
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2024-08-09 11:40:41 +0000

    Sync LeetCode submission Runtime - 50 ms (28.72%), Memory - 49.2 MB (100.00%)

---
+++ solutions/2761-array-reduce-transformation/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+
+    let accm = init;
+
+    for(var i=0;i<nums.length;i++){
+    accm = fn(accm,nums[i]);
+       
+    }
+    return accm;
+    
+};
