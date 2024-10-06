b23917af6337ea2a2f5a6436308e4c613f3c8b1a
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2024-10-06 10:25:17 +0000

    Sync LeetCode submission Runtime - 56 ms (5.52%), Memory - 49.7 MB (100.00%)

---
+++ solutions/2864-is-object-empty/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    
+   
+    if(!Object.keys(obj).length){
+        return true;
+    }else{
+        return false;
+    }
+};
