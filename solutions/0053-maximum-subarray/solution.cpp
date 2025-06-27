93d84e19893c780b461797c4d9c60e31fdfbbd48
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2025-06-27 06:06:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.8 MB (51.96%)

---
+++ solutions/0053-maximum-subarray/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr_max = nums[0];
+        int res = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            curr_max = max(nums[i],nums[i]+curr_max);
+            res = max(res,curr_max);
+        }
+        return res;
+        
+    }
+};
