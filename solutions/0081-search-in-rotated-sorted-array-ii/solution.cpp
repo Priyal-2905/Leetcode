47aa41878b26668b736a76b12ed07fe4d17181f5
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2026-01-04 17:44:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (77.32%)

--- solutions/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ solutions/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() -1 ;
+     int low = 0, high = nums.size() - 1;
 
-        while(low<=high){
-            int mid = (low +high)/2;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-            if(nums[mid] == target)
-              return true;
+        if (nums[mid] == target)
+            return true;
 
-             if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+        if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
             low++;
             high--;
-            }
-            else if(nums[low] <= nums[mid])
-            {
-                if(nums[low]<=target && nums[mid]>=target)
-                    high = mid-1;
-                else if(nums[low]==target)
-                    return true;
-                else
-                 low = mid+1;
-            }
-            else{
-                if(nums[high]>=target && nums[mid]<=target)
-                    low = mid+1;
-                else if(nums[high]==target)
-                    return true;
-                else
-                 high = mid-1;
-            }
-            
+        }
+    
+        else if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else {
+            if (nums[mid] < target && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
         }
         return false;
     }
