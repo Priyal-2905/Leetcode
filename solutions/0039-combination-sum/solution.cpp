56022b3746edd3849898a9480971d6542083305e
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2026-01-11 02:44:49 +0000

    Sync LeetCode submission Runtime - 3 ms (52.55%), Memory - 14.1 MB (57.26%)

--- solutions/0039-combination-sum/solution.cpp
+++ solutions/0039-combination-sum/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> comb;
-        vector<vector<int>> res;
-
-        combo(candidates,target,0,0,comb,res);
-        return res;       
-    }
-
-    void combo(vector<int>& candidates,int target,int total,int index,vector<int> comb, vector<vector<int>>& res){
-
-        if(total == target){
-            res.push_back(comb);
+    void solve(vector<int>& curr,vector<vector<int>>& res,vector<int>& candidates,int target,int i){
+        if(target == 0){
+            res.push_back(curr);
             return;
         }
-        if(total > target || index >= candidates.size()){ return; }
 
-        comb.push_back(candidates[index]);
-        combo(candidates,target,total+candidates[index],index,comb,res);
-        comb.pop_back();
-        combo(candidates,target,total,index+1,comb,res);
+        if(target < 0||i >= candidates.size()){
+            return;
+        }
 
+        curr.push_back(candidates[i]);
+        solve(curr,res,candidates,target - candidates[i],i);
+        curr.pop_back();
+        solve(curr,res,candidates,target,i+1);
     }
-    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        solve(curr,res,candidates,target,0);
+        return res;
+    }
 };
