69923d124a8e2cfe2c73cda2bec7b330526e21c1
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2026-01-12 05:38:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.8 MB (58.25%)

---
+++ solutions/1395-minimum-time-visiting-all-points/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int res = 0;
+
+        for (int i = 1; i < points.size(); i++) {
+            int x1 = abs(points[i][0] - points[i - 1][0]);
+            int y1 = abs(points[i][1] - points[i - 1][1]);
+            res += max(x1, y1);
+         }
+
+        return res;
+    }
+};
