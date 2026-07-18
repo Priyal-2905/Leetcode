32681110b544da6f2b4841c0f02322bf734accfb
Priyal Khandal <108855412+Priyal-2905@users.noreply.github.com>
2026-07-18 02:26:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16 MB (92.83%)

---
+++ solutions/2106-find-greatest-common-divisor-of-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int gcd(int a,int b){
+        if(b == 0){
+            return a;
+        }
+
+        return gcd(b,a%b);
+    }
+
+    int findGCD(vector<int>& nums) {
+        int small = *min_element(nums.begin(),nums.end());
+        int large = *max_element(nums.begin(),nums.end());
+
+        return gcd(large,small);
+    }
+};
